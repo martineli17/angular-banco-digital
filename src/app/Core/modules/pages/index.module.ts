4b20f4a871e11732aeadc413ab48c421699aa49a
felipearantes <felipe.arantes05@gmail.com>
2021-05-16 11:10:37 -0300

    Alterações do merge

--- src/app/Core/modules/pages/index.module.ts
+++ src/app/Core/modules/pages/index.module.ts
@@ -6,14 +6,11 @@ import {RegisterModule} from "../components/register.module";
 
 @NgModule({
     declarations:[IndexPage],
-<<<<<<< HEAD
     imports:[
         LoginModule,
-        RegisterModule
+        RegisterModule,
+        CartaoModule
     ]
-=======
-    imports:[LoginModule, CartaoModule]
->>>>>>> 88ccb382b2b2b69f50bb560e9c07745637b3e6d4
 })
 export class IndexModule{
 
