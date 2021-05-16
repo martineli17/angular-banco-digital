944ed3b81d268d67fff3ae86ae0014b8f4572b17
felipearantes <felipe.arantes05@gmail.com>
2021-05-16 15:22:24 -0300

    Alterando css e msg erro tela de login

--- src/app/Components/user/login/login.component.ts
+++ src/app/Components/user/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ValidateErrorsFormService } from "src/app/Shared/form/validador-form";
+import { ValidadorErrosKeys, ValidadorKeys } from "src/app/Shared/model/validador-key.model";
 
 @Component({
     templateUrl: "./login.component.html",
@@ -8,9 +11,33 @@ import { Component } from "@angular/core";
         "./login.component.css",
     ]
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit{
     cpfCnpj: string = "";
     senha: string = "";
-    constructor() {
+
+    public form: FormGroup = {} as FormGroup;
+    public errosMsg: ValidadorErrosKeys[] = [];
+    
+    constructor(private formBuilder: FormBuilder, public validatorMsg: ValidateErrorsFormService) {
     }
+
+    ngOnInit(){
+        this.CreateForm();
+    }
+
+    private CreateForm(): void{
+        this.form = this.formBuilder.group(
+           {Cpf: ["", [Validators.required, Validators.maxLength(11)]],
+            Senha: ["", [Validators.required]]});
+    }
+
+    FazerLogin(): void{
+        alert("Ainda não foi implementado!!!")
+    }
+
+    private CreateKeysLabelsErrors = (): ValidadorKeys[] =>
+    [
+        { Label: "CPF", Name: "Cpf"},
+        { Label: "Senha", Name: "Senha"}
+    ]
 }
